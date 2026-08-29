3e19626387ce1f248061d082cd829705aa9e664a
hsoprasnd-creator <hsoprasnd@gmail.com>
2026-08-29 13:06:45 +0530

    Update imconfig.h

--- imgui/imconfig.h
+++ imgui/imconfig.h
@@ -6,23 +6,20 @@
 
 #pragma once
 
-// ============================================
-// FIX: Force C++17 and disable constexpr issues for Objective-C++
-// ============================================
-#if defined(__OBJC__) || defined(__OBJC_CPP__)
-    #define IMGUI_USE_STD_ALGORITHMS
-    #define IMGUI_DISABLE_OBSOLETE_FUNCTIONS
-    #define IMGUI_DISABLE_DEMO_WINDOWS
-#endif
-
-// ============================================
-// REST OF YOUR ORIGINAL imconfig.h
-// ============================================
-
 //---- Define assertion handler. Defaults to calling assert().
-// - If your macro uses multiple statements, make sure is enclosed in a 'do { .. } while (0)' block so it can be used as a single statement.
-// - Compiling with NDEBUG will usually strip out assert() to nothing, which is NOT recommended because we use asserts to notify of programmer mistakes.
 //#define IM_ASSERT(_EXPR)  MyAssert(_EXPR)
 //#define IM_ASSERT(_EXPR)  ((void)(_EXPR))     // Disable asserts
 
-// ... BAQI AAPKI EXISTING imconfig.h CODE ...
+//---- Define attributes of all API symbols declarations, e.g. for DLL under Windows
+//#define IMGUI_API __declspec(dllexport)
+//#define IMGUI_API __declspec(dllimport)
+
+//---- Don't define obsolete functions/enums/names.
+#define IMGUI_DISABLE_OBSOLETE_FUNCTIONS
+#define IMGUI_DISABLE_OBSOLETE_KEYIO
+
+//---- Disable standard features to save code size
+#define IMGUI_DISABLE_DEMO_WINDOWS
+
+//---- Include standard types
+#include <stdint.h>
