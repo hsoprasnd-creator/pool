8c347f8411211e941377bf171369e29fd4a97e31
hsoprasnd-creator <hsoprasnd@gmail.com>
2026-08-29 14:43:46 +0530

    Update main.mm

--- main.mm
+++ main.mm
@@ -1,95 +1,91 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
+#import <CoreGraphics/CoreGraphics.h>
 #import <objc/runtime.h>
-#import <objc/message.h>
+#import <substrate.h>
 
-// Global hooks references
+// Global variables export (Prediction Engine ke liye)
 double g_liveAimAngle = 0.0;
-id __weak g_tableInstance = nil;
-id __weak g_ballManagerInstance = nil;
+id g_tableInstance = nil;
+id g_ballManagerInstance = nil;
 
-typedef struct { float x; float y; float width; float height; } MCRect;
-MCRect g_tableBounds = {100.0f, 100.0f, 700.0f, 400.0f};
-
-// Method Swizzling Helper
-static void SafeSwizzle(Class cls, SEL origSel, SEL newSel, IMP newImp, const char *types) {
-    if (!cls) return;
-    class_addMethod(cls, newSel, newImp, types);
-    Method origMethod = class_getInstanceMethod(cls, origSel);
-    Method newMethod = class_getInstanceMethod(cls, newSel);
-    if (origMethod && newMethod) {
-        method_exchangeImplementations(origMethod, newMethod);
-    }
-}
-
-// 1. Hook VisualCue
-static void swizzled_VisualCue_setAimAngle(id self, SEL _cmd, void *mcNumberPtr) {
-    if (mcNumberPtr != NULL) {
+// 1. Hook VisualCue setAimAngle:
+static void (*orig_VisualCue_setAimAngle)(id self, SEL _cmd, void *mcNumberPtr);
+static void hook_VisualCue_setAimAngle(id self, SEL _cmd, void *mcNumberPtr) {
+    if (mcNumberPtr) {
         g_liveAimAngle = *(double *)mcNumberPtr;
     }
-    SEL origSel = @selector(swizzled_VisualCue_setAimAngle:);
-    if ([self respondsToSelector:origSel]) {
-        ((void(*)(id, SEL, void*))objc_msgSend)(self, origSel, mcNumberPtr);
+    orig_VisualCue_setAimAngle(self, _cmd, mcNumberPtr);
+}
+
+// 2. Hook BallManager getBallPositionForNumber:
+static CGPoint (*orig_BallManager_getBallPositionForNumber)(id self, SEL _cmd, unsigned int num);
+static CGPoint hook_BallManager_getBallPositionForNumber(id self, SEL _cmd, unsigned int num) {
+    g_ballManagerInstance = self;
+    return orig_BallManager_getBallPositionForNumber(self, _cmd, num);
+}
+
+// 3. Hook Table tableBounds
+typedef struct { float x; float y; float width; float height; } MCRect;
+static MCRect (*orig_Table_tableBounds)(id self, SEL _cmd);
+static MCRect hook_Table_tableBounds(id self, SEL _cmd) {
+    g_tableInstance = self;
+    return orig_Table_tableBounds(self, _cmd);
+}
+
+// Separate function to safely inject hooks after security check passes
+void InjectPoolHooksSafely() {
+    NSLog(@"[PoolPrediction] Starting delayed hook injection...");
+
+    // VisualCue Hook
+    Class VisualCueClass = objc_getClass("VisualCue");
+    if (VisualCueClass) {
+        MSHookMessageEx(VisualCueClass, 
+                        @selector(setAimAngle:), 
+                        (IMP)hook_VisualCue_setAimAngle, 
+                        (IMP*)&orig_VisualCue_setAimAngle);
+        NSLog(@"[PoolPrediction] VisualCue Hook Applied.");
+    }
+
+    // BallManager Hook
+    Class BallManagerClass = objc_getClass("BallManager");
+    if (BallManagerClass) {
+        MSHookMessageEx(BallManagerClass, 
+                        @selector(getBallPositionForNumber:), 
+                        (IMP)hook_BallManager_getBallPositionForNumber, 
+                        (IMP*)&orig_BallManager_getBallPositionForNumber);
+        NSLog(@"[PoolPrediction] BallManager Hook Applied.");
+    }
+
+    // Table Hook
+    Class TableClass = objc_getClass("Table");
+    if (TableClass) {
+        MSHookMessageEx(TableClass, 
+                        @selector(tableBounds), 
+                        (IMP)hook_Table_tableBounds, 
+                        (IMP*)&orig_Table_tableBounds);
+        NSLog(@"[PoolPrediction] Table Hook Applied.");
     }
 }
 
-// 2. Hook BallManager
-static CGPoint swizzled_BallManager_getBallPositionForNumber(id self, SEL _cmd, unsigned int num) {
-    if (self) {
-        g_ballManagerInstance = self;
-    }
-    SEL origSel = @selector(swizzled_BallManager_getBallPositionForNumber:);
-    if ([self respondsToSelector:origSel]) {
-        return ((CGPoint(*)(id, SEL, unsigned int))objc_msgSend)(self, origSel, num);
-    }
-    return CGPointZero;
-}
-
-// 3. Hook Table Bounds
-static MCRect swizzled_Table_tableBounds(id self, SEL _cmd) {
-    if (self) {
-        g_tableInstance = self;
-        SEL origSel = @selector(swizzled_Table_tableBounds);
-        if ([self respondsToSelector:origSel]) {
-            g_tableBounds = ((MCRect(*)(id, SEL))objc_msgSend)(self, origSel);
-        }
-    }
-    return g_tableBounds;
-}
-
-extern void InitImGuiHook();
-
-// Constructor
+// 4. Delayed Execution on Dylib Load (Anti-Cheat Bypass)
 __attribute__((constructor))
 static void InitPoolDylib() {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        Class VisualCueClass = objc_getClass("VisualCue");
-        if (VisualCueClass) {
-            SafeSwizzle(VisualCueClass, 
-                        @selector(setAimAngle:), 
-                        @selector(swizzled_VisualCue_setAimAngle:), 
-                        (IMP)swizzled_VisualCue_setAimAngle, 
-                        "v@:^v");
-        }
+    NSLog(@"[PoolPrediction] Dylib Injected. Waiting for security checks to clear...");
 
-        Class BallManagerClass = objc_getClass("BallManager");
-        if (BallManagerClass) {
-            SafeSwizzle(BallManagerClass, 
-                        @selector(getBallPositionForNumber:), 
-                        @selector(swizzled_BallManager_getBallPositionForNumber:), 
-                        (IMP)swizzled_BallManager_getBallPositionForNumber, 
-                        "{CGPoint=dd}@:I");
-        }
-
-        Class TableClass = objc_getClass("Table");
-        if (TableClass) {
-            SafeSwizzle(TableClass, 
-                        @selector(tableBounds), 
-                        @selector(swizzled_Table_tableBounds), 
-                        (IMP)swizzled_Table_tableBounds, 
-                        "{MCRect=ffff}@:");
-        }
-
-        InitImGuiHook();
+    // Background thread use karenge taaki main UI thread freeze na ho aur app crash na kare
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        
+        // 12-15 seconds ka delay, taaki game fully load ho jaye aur anti-cheat check khatam ho jaye
+        [NSThread sleepForTimeInterval:12.0];
+        
+        // Hooks ko main thread par wapas execute karenge safety ke liye
+        dispatch_async(dispatch_get_main_queue(), ^{
+            @try {
+                InjectPoolHooksSafely();
+            } @catch (NSException *exception) {
+                NSLog(@"[PoolPrediction] Error occurred during injection: %@", exception.reason);
+            }
+        });
     });
 }
