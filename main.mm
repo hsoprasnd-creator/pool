0bec7e1e076128b9696c80bd32b375eb7a57c719
hsoprasnd-creator <hsoprasnd@gmail.com>
2026-08-29 11:23:04 +0530

    Update main.mm

--- main.mm
+++ main.mm
@@ -1,9 +1,8 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 #import <objc/runtime.h>
-#import <substrate.h>
 
-// Safe Global references
+// Global hooks references
 double g_liveAimAngle = 0.0;
 id __weak g_tableInstance = nil;
 id __weak g_ballManagerInstance = nil;
@@ -11,54 +10,84 @@ id __weak g_ballManagerInstance = nil;
 typedef struct { float x; float y; float width; float height; } MCRect;
 MCRect g_tableBounds = {100.0f, 100.0f, 700.0f, 400.0f};
 
-// 1. Safe Hook AimAngle
-static void (*orig_VisualCue_setAimAngle)(id self, SEL _cmd, void *mcNumberPtr);
-static void hook_VisualCue_setAimAngle(id self, SEL _cmd, void *mcNumberPtr) {
+// Method Swizzling Helper (Substrate-Free Bypass)
+static void SafeSwizzle(Class cls, SEL origSel, SEL newSel, IMP newImp, const char *types) {
+    if (!cls) return;
+    class_addMethod(cls, newSel, newImp, types);
+    Method origMethod = class_getInstanceMethod(cls, origSel);
+    Method newMethod = class_getInstanceMethod(cls, newSel);
+    if (origMethod && newMethod) {
+        method_exchangeImplementations(origMethod, newMethod);
+    }
+}
+
+// 1. Hook VisualCue
+static void swizzled_VisualCue_setAimAngle(id self, SEL _cmd, void *mcNumberPtr) {
     if (mcNumberPtr != NULL) {
         g_liveAimAngle = *(double *)mcNumberPtr;
     }
-    orig_VisualCue_setAimAngle(self, _cmd, mcNumberPtr);
+    // Call original implementation via renamed selector
+    SEL origSel = @selector(swizzled_VisualCue_setAimAngle:);
+    if ([self respondsToSelector:origSel]) {
+        ((void(*)(id, SEL, void*))objc_msgSend)(self, origSel, mcNumberPtr);
+    }
 }
 
-// 2. Safe Hook BallManager
-static CGPoint (*orig_BallManager_getBallPositionForNumber)(id self, SEL _cmd, unsigned int num);
-static CGPoint hook_BallManager_getBallPositionForNumber(id self, SEL _cmd, unsigned int num) {
+// 2. Hook BallManager
+static CGPoint swizzled_BallManager_getBallPositionForNumber(id self, SEL _cmd, unsigned int num) {
     if (self) {
         g_ballManagerInstance = self;
     }
-    return orig_BallManager_getBallPositionForNumber(self, _cmd, num);
+    SEL origSel = @selector(swizzled_BallManager_getBallPositionForNumber:);
+    if ([self respondsToSelector:origSel]) {
+        return ((CGPoint(*)(id, SEL, unsigned int))objc_msgSend)(self, origSel, num);
+    }
+    return CGPointZero;
 }
 
-// 3. Safe Hook Table Bounds
-static MCRect (*orig_Table_tableBounds)(id self, SEL _cmd);
-static MCRect hook_Table_tableBounds(id self, SEL _cmd) {
+// 3. Hook Table Bounds
+static MCRect swizzled_Table_tableBounds(id self, SEL _cmd) {
     if (self) {
         g_tableInstance = self;
-        g_tableBounds = orig_Table_tableBounds(self, _cmd);
+        SEL origSel = @selector(swizzled_Table_tableBounds);
+        if ([self respondsToSelector:origSel]) {
+            g_tableBounds = ((MCRect(*)(id, SEL))objc_msgSend)(self, origSel);
+        }
     }
     return g_tableBounds;
 }
 
-// External hook from ImGuiHook.mm
 extern void InitImGuiHook();
 
-// Constructor with Safe Class Checking
+// Delayed initialization to let game load security checks first
 __attribute__((constructor))
 static void InitPoolDylib() {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         Class VisualCueClass = objc_getClass("VisualCue");
         if (VisualCueClass) {
-            MSHookMessageEx(VisualCueClass, @selector(setAimAngle:), (IMP)hook_VisualCue_setAimAngle, (IMP*)&orig_VisualCue_setAimAngle);
+            SafeSwizzle(VisualCueClass, 
+                        @selector(setAimAngle:), 
+                        @selector(swizzled_VisualCue_setAimAngle:), 
+                        (IMP)swizzled_VisualCue_setAimAngle, 
+                        "v@:^v");
         }
 
         Class BallManagerClass = objc_getClass("BallManager");
         if (BallManagerClass) {
-            MSHookMessageEx(BallManagerClass, @selector(getBallPositionForNumber:), (IMP)hook_BallManager_getBallPositionForNumber, (IMP*)&orig_BallManager_getBallPositionForNumber);
+            SafeSwizzle(BallManagerClass, 
+                        @selector(getBallPositionForNumber:), 
+                        @selector(swizzled_BallManager_getBallPositionForNumber:), 
+                        (IMP)swizzled_BallManager_getBallPositionForNumber, 
+                        "{CGPoint=dd}@:I");
         }
 
         Class TableClass = objc_getClass("Table");
         if (TableClass) {
-            MSHookMessageEx(TableClass, @selector(tableBounds), (IMP)hook_Table_tableBounds, (IMP*)&orig_Table_tableBounds);
+            SafeSwizzle(TableClass, 
+                        @selector(tableBounds), 
+                        @selector(swizzled_Table_tableBounds), 
+                        (IMP)swizzled_Table_tableBounds, 
+                        "{MCRect=ffff}@:");
         }
 
         InitImGuiHook();
